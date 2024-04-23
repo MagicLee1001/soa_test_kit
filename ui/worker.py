b1e20b8de499e23eb85ae7e527c4c63ff6000cfe
MagicLee1001 <41740090+MagicLee1001@users.noreply.github.com>
2024-04-23 18:31:05 +0800

    Create worker.py

---
+++ ui/worker.py
@@ -0,0 +1,219 @@
+# -*- coding: utf-8 -*-
+# @Author  : Li Kun
+# @Time    : 2024/3/29 14:15
+# @File    : worker.py
+
+import os
+import time
+import json
+import traceback
+from runner.log import logger
+from PyQt5 import QtCore
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.Qt import QThread, QObject
+from settings import env
+from connector.sdc import SDCConnector
+from connector.dds import DDSConnector, DDSConnectorRti
+from runner.tester import CaseTester, TestPrecondition, TestPostCondition, TestHandle
+from runner.assistant import HandleTestCaseFile
+from test_framework import set_test_handle, qt_main
+
+
+# 在Qt框架中，几乎所有的GUI组件，都不是线程安全的。
+# 这就意味着你不能在除主线程之外的任何线程中操作它们。
+# 如果你试图在其他线程中操作GUI组件，可能会导致不可预见的行为，包括闪退、数据错误和死锁等。
+# 应该使用信号和槽机制来将内容发送给主线程，并在槽函数中更新GUI
+
+
+class GetGlobalVarsWorker(QThread):
+    """
+    线程安全，QT线程，获取vars_mapping并传递给其他ui线程使用
+    todo: 后期有界面闪退再用这种方式，目前全局变量上锁认为是安全的
+    """
+    pass
+
+
+class AutoTestWorker(QThread):
+    """ 自动化测试线程"""
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+
+    def run(self) -> None:
+        try:
+            # TestHandle变量重置
+            set_test_handle()
+            # 设置执行用例
+            self.app.set_env_testcase()
+            # 开始自动化测试
+            qt_main()
+            # 飞书报告通知
+            TestHandle.feishu_notice()
+            if getattr(env, 'stop_autotest', True):
+                TestHandle.run_state = '已停止'
+                time.sleep(0.5)
+                return
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            TestHandle.run_state = '执行异常: ' + str(e)
+        else:
+            TestHandle.run_state = '已结束'
+            time.sleep(1)
+        finally:
+            logger.info('测试任务执行全部完成 资源重置')
+            env.stop_autotest = False
+            if env.remote_callback:
+                env.remote_callback.task_callback(env.xcu_info)
+            # 远程信息重置
+            env.remote_event_data = None
+            env.remote_callback = None
+
+
+class ReloadSettingWorker(QThread):
+    """ 配置重加载线程 当前测试对象不能变化"""
+    display_case_path_signal = pyqtSignal(object)
+
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+
+    def run(self) -> None:
+        try:
+            # 处理空配置文件路径
+            setting_filepath = self.app.setting_file.text()
+            if not setting_filepath:
+                setting_filepath = env.settings_filepath
+            env.load(setting_filepath)
+            # 更新配置中的用例并渲染到UI
+            case_filenames = TestHandle.get_filename_from_dir(env.case_dir, 'xlsm')
+            case_filepaths = [os.path.join(env.case_dir, case_filename) for case_filename in case_filenames]
+            self.display_case_path_signal.emit(case_filepaths)
+            # 重写初始化connector 更新各种信号矩阵
+            env.sdc_connector = SDCConnector(env.dbo_filepath, server_ip=env.sil_server_ip, server_port=env.sil_server_port)
+            if 'rti_' in env.idl_filepath.lower():
+                env.DDSConnectorClass = DDSConnectorRti
+                env.platform_version = 2.0
+            else:
+                env.DDSConnectorClass = DDSConnector
+                env.platform_version = 2.5
+            env.dds_connector = env.DDSConnectorClass(idl_filepath=env.idl_filepath)
+            env.tester = CaseTester(
+                sub_topics=env.sub_topics,
+                pub_topics=env.pub_topics,
+                sdc_connector=env.sdc_connector,
+                dds_connector=env.dds_connector,
+                ssh_connector=env.ssh_connector,
+                doip_simulator=env.doip_simulator,
+            )
+            tpr = TestPrecondition(env.tester)
+            # 更新ssh信号矩阵
+            tpr.start_ssh_connector()
+            # 更新dds writer与reader池
+            tpr.start_dds_connector()
+            # 重新连接sil server
+            env.sdc_connector.reconnect_server()
+            logger.success('环境配置更新完成')
+        except:
+            logger.error(traceback.format_exc())
+
+
+class ModifyConfigWordWorker(QThread):
+    """ 修改配置字线程"""
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+
+    def run(self) -> None:
+        try:
+            text = self.app.cw_edit.text()
+            if text:
+                result = env.tester.ssh_connector.modify_config_word_file(text)
+                if result:
+                    env.xcu_info['config_word'] = text
+                # 这里由于改完后重启,需要主动重新连接一下sil仿真程序
+                env.tester.sdc_connector.reconnect_server()
+        except:
+            logger.error(traceback.format_exc())
+
+
+class RecoverEnvironment(QThread):
+    """ 测试环境还原线程"""
+    enable_recover_tool_signal = pyqtSignal(bool)
+
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+        self.enable_recover_tool_signal.connect(self.app.recover_tool.setEnabled)
+
+    def run(self) -> None:
+        try:
+            # 发送信号以使按钮置灰（False代表不可用）
+            self.enable_recover_tool_signal.emit(False)
+            # 执行恢复操作
+            env.tester.ssh_connector.recover_sil_environment()
+        except:
+            logger.error(traceback.format_exc())
+        finally:
+            # 恢复过程完成后，发送信号以使按钮可用（True代表可用）
+            self.enable_recover_tool_signal.emit(True)
+
+
+class ReleaseWorker(QThread):
+    """
+    关闭窗口时，释放资源的线程
+    """
+    def __init__(self, app):
+        super().__init__()
+        self.app = app
+
+    def run(self) -> None:
+        """
+        执行测试后的清理工作
+        """
+        try:
+            env.tester.ssh_connector.setup_vcs_ip_config(0)
+            TestPostCondition(env.tester).run()
+            logger.info('资源释放完成')
+        except:
+            logger.error(traceback.format_exc())
+
+
+class SafeLogHandler(QtCore.QObject):
+    new_log = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+
+    def write(self, message):
+        try:
+            self.new_log.emit(message.strip())
+        except:
+            pass
+
+    def flush(self):
+        pass
+
+
+class GenTestCaseWorker(QThread):
+    def __init__(self, app, source_path, project):
+        super().__init__()
+        self.app = app
+        self.source_path = source_path
+        self.project = project
+
+    def run(self):
+        HandleTestCaseFile.gen_test_case(self.source_path, self.project)
+
+
+class LowCaseTransWorker(QThread):
+    display_paths_signal = pyqtSignal(object)
+
+    def __init__(self, app, source_path, save_dir=''):
+        super().__init__()
+        self.app = app
+        self.source_path = source_path
+        self.save_dir = save_dir
+
+    def run(self):
+        paths = HandleTestCaseFile.lowercase_trans(self.source_path, save_dir=self.save_dir)
+        self.display_paths_signal.emit(paths)  # 发出显示最新用例路径的信号

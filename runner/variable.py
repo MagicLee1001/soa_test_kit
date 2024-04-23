b2a19848ecb4c1f103dbccafcda5e8a9f22388c1
MagicLee1001 <41740090+MagicLee1001@users.noreply.github.com>
2024-04-23 18:29:15 +0800

    Update variable.py

--- runner/variable.py
+++ runner/variable.py
@@ -3,64 +3,57 @@
 # @Time    : 2023/10/19 18:13
 # @File    : variable.py
 
-
 import threading
-from loguru import logger
-
-vars_mapping = {}
+from collections import deque
 
 
 class Variable:
-    # vars_mapping = {}
-    _lock = threading.Lock()
-    _lock_variable = threading.Lock()
+    _vars_mapping = {}  # 这个变量不建议直接操作，全部通过类方法进行访问，否则线程不安全
+    _lock = threading.Lock()  # 锁 _vars_mapping
+    _lock_variable = threading.Lock()  # 锁 Variable
 
     def __new__(cls, name, value=0):
         with cls._lock:
-            global vars_mapping
-            if name in vars_mapping:
-                return vars_mapping[name]
-            else:
-                instance = super().__new__(cls)
-                instance.name = name
-                instance.data_array = [value]
-                instance.index = 0
-                instance.pre_index = 0
-                instance._value = [value]
-                instance._pre_value = []
-                vars_mapping[name] = instance
-                return instance
+            if name in cls._vars_mapping:
+                return cls._vars_mapping[name]
+            instance = super().__new__(cls)
+            instance.name = name
+            instance.data_array = deque([value], maxlen=3)
+            instance.index = 0
+            cls._vars_mapping[name] = instance
+            return instance
 
     def _var(self, name):
-        global vars_mapping
-        return vars_mapping.get(name)
+        with Variable._lock:
+            return Variable._vars_mapping.get(name)
+
+    @classmethod
+    def check_existence(cls, name):
+        with cls._lock:
+            return name in cls._vars_mapping
+
+    @classmethod
+    def get_var_keys(cls):
+        with cls._lock:
+            return cls._vars_mapping.keys()
 
     @property
     def Value(self):
-        return self.data_array[-1]
-
-    @property
-    def preValue(self):
-        if len(self._pre_value) == 0:
-            return None
-        return self._pre_value[-1]
+        with self._lock_variable:
+            return self.data_array[-1]
 
     @Value.setter
     def Value(self, new_value):
         with self._lock_variable:
-            global vars_mapping
-            vars_mapping.get(self.name).data_array.append(new_value)
+            self.data_array.append(new_value)
 
-    def update_pre_value(self):
-        self._pre_value.append(self._value[-1])
 
-    def update_value(self):
-        if len(self.data_array) <= 1:
-            return
-        self.index += 1
-        try:
-            v = self.data_array[self.index]
-            self._value.append(v)
-        except Exception as e:
-            logger.error(f'error: {str(e)}')
-            
+if __name__ == '__main__':
+    pass
+    Variable('SOA', 0)
+    print(Variable('SOA').Value)
+    Variable('SOA').Value = 1
+    print(Variable('SOA').Value)
+    print(Variable.check_existence('SOA'))
+    print(Variable.check_existence('SOA2'))
+    print(Variable.get_var_keys())

f1066c51f9946b4e404a3f73f343b7f558d5f115
likun <41740090+MagicLee1001@users.noreply.github.com>
2025-05-27 13:57:52 +0800

    Update __init__.py

--- connector/__init__.py
+++ connector/__init__.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 # @Author  : Li Kun
+# @Email   : likun3@lixiang.com
 # @Time    : 2023/10/19 17:29
-# @File    : __init__.py
+# @File    : __init__.py.py
+
+from .database import *
+from .dds import *
+from .doipclient import *
+from .sdc import *
+from .ssh import *
+from .xcp import *
 
 
-class ConnectorPool:
-    dds_reader_pool = {}
-    dds_writer_pool = {}
-    topic_obj_pool = {}
+__all__ = [
+    'DBConnector', 'DDSConnector', 'DDSConnectorRti', 'DoIPClient', 'SDCConnector',
+    'SSHConnector', 'SSHAsyncConnector', 'XCPConnector'
+]
